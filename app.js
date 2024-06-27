8a90210d196a6c92c72a742f362e1a87800494f1
nadeemk2023 <nadeemekhan@gmail.com>
2024-06-26 23:55:34 -0700

    Refactor variable name for better readability

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('testing link to html file');
+const name = 'Ned';
+
+//!just like arrays strings can be accessed by index, let's do an example in the console
+// console.log(name[0]);
+
+//!now let's print the lowercase d of the below string as an exercise
+// console.log('Hello World'[10]);
+
+//!for a more dynamic approach to the above example, we can use the length property of strings once it is stored in a variable
+const greeting = 'Hello World';
+// console.log(greeting[greeting.length - 1]);
+
+//! If you are ever confused on the data type of a variable, you can use the typeof operator to check
+// console.log(typeof 5);
+
+//! Variables are used to store value, in programming it is importan to be descriptive of the value stored in a variable
+/* For instance, if you have a variable that stores a name, you should name the variable name, if it stores a number, you should name the variable number, if it stores a boolean, you should name the variable isTrue or hasValue etc. 
+
+This is important because it makes your code more readable and easier to understand for other developers who may work on your code in the future.
+example:
+let x = 5; // not descriptive
+let age = 5; // descriptive
+*/
+let username = 'Ned Stark';
